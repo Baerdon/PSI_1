06fa3f00596c87d1c4caa687b1ff549e3650f0b4
Baerdon <jardabazant@seznam.cz>
2018-02-25 18:26:54 +0100

    Initial Commit

---
+++ PSI_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PSI_1
+//
+//  Created by Jaroslav Bažant on 25.02.18.
+//  Copyright © 2018 Jaroslav Bažant. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
